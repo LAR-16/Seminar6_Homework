557ca3da2b20622d785e938086b3367d2b3307d1
Alexandra <lebedeva1618@yandex.ru>
2022-12-16 16:11:03 +0300

    задача 41, сколько чисел больше нуля

---
+++ task41/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+Console.WriteLine("Ведите ряд чисел: ");
+int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+    if( array[i] > 0)
+        count++;
+Console.WriteLine($"Количество чисел больше нуля = {count} ");
