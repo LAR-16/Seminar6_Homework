982799f1c1ded9740d95bb288ba967d1661eef28
Alexandra <lebedeva1618@yandex.ru>
2022-12-16 17:03:33 +0300

    задача 43, координата точек пересечения

---
+++ task43/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Clear();
+Console.WriteLine("Введите переменную k1: ");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите переменную b1: ");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите переменную k2: ");
+double k2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите переменную b2: ");
+double b2 = double.Parse(Console.ReadLine());
+
+double x = (b2-b1)/(k1-k2);
+
+double y = k1 * x + b1;
+Console.WriteLine($"Координаты точки пересечения -> ({x};{y})");
