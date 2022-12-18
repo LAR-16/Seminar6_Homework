9bea6ce1228c9ce10f211f52f5bc8e7b631b7867
Alexandra <lebedeva1618@yandex.ru>
2022-12-18 16:35:08 +0300

    переименовали стороны треугольника

--- Sabc/Program.cs
+++ Sabc/Program.cs
@@ -13,9 +13,9 @@ int x3 = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите y3: ");
 int y3 = int.Parse(Console.ReadLine());
 
-double A = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2));
-double B = Math.Sqrt(Math.Pow((x3-x2), 2) + Math.Pow((y3-y2), 2));
-double C = Math.Sqrt(Math.Pow((x1-x3), 2) + Math.Pow((y1-y3), 2));
-double p = (A + B + C) / 2;
-double S = Math.Round(Math.Sqrt(p*(p-A)*(p-B)*(p-C)), 2);
+double AB = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2));
+double BC = Math.Sqrt(Math.Pow((x3-x2), 2) + Math.Pow((y3-y2), 2));
+double CA = Math.Sqrt(Math.Pow((x1-x3), 2) + Math.Pow((y1-y3), 2));
+double p = (AB + BC + CA) / 2;
+double S = Math.Round(Math.Sqrt(p*(p-AB)*(p-BC)*(p-CA)), 2);
 Console.WriteLine($"Площадь треугольника = {S}");
